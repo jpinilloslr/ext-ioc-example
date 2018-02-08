959a37eed5c7c9809505017e70c812afe46c119e
Joaquin <jpinilloslr@gmail.com>
2018-02-07 23:35:57 -0300

    Add dependency resolver to WebApi project.

--- ExtIocExample.DistributedServices.WebApi/App_Start/WebApiConfig.cs
+++ ExtIocExample.DistributedServices.WebApi/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Infrastructure.Crosscutting.IoC.Seedwork;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace ExtIocExample.DistributedServices.WebApi
 {
@@ -16,6 +12,7 @@ namespace ExtIocExample.DistributedServices.WebApi
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.DependencyResolver = Bootstrapper.DependencyContainer;
 
             // Web API routes
             config.MapHttpAttributeRoutes();
